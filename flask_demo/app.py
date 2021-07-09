2b8397854d4c5e07ab968fbb249e378a0d589bc0
alevere <alevere@email.unc.edu>
2021-07-09 16:24:12 -0400

    Update app.py

--- flask_demo/app.py
+++ flask_demo/app.py
@@ -211,7 +211,7 @@ def verify_assertion():
     challenge = session.get('challenge')
     assertion_response = request.form
     credential_id = assertion_response.get('id')
-    print assertion_response
+    print(assertion_response)
     user = User.query.filter_by(credential_id=credential_id).first()
     if not user:
         return make_response(jsonify({'fail': 'User does not exist.'}), 401)
